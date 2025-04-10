caeef92bf2e15fa941057da53a1e46336bae329c
Mati Araya <matiasaraya.ig01@gmail.com>
2025-04-09 23:49:45 -0400

    navbar en proyectos.html

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("partials/navbar.html") // sin la barra al inicio
+    fetch("partials/navbar.html") 
       .then(response => response.text())
       .then(data => {
         document.getElementById("navbar-container").innerHTML = data;
