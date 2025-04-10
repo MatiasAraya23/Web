b685505aa0ad1625fc76c15f2f761bd7a6dd3d8a
Mati Araya <matiasaraya.ig01@gmail.com>
2025-04-10 02:22:37 -0400

    Version index.html ultimo

--- static/js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("../partials/navbar.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("navbar-container").innerHTML = data;
-    })
-    .catch(err => console.error("Error al cargar navbar:", err));
-});
